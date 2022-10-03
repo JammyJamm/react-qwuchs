416b23f7b3de0efbc32800baf58f18803454a602
JammyJamm <59868544+JammyJamm@users.noreply.github.com>
2022-10-03 09:41:36 +0530

    3 files modified, 23 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import './style.css';
-
-export default function App() {
+import "./App.css";
+import React from "react";
+import DataLayout from "./project/DataLayout";
+import Learning from "./learning/Learning";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "jquery/dist/jquery.min.js";
+// import "bootstrap/dist/js/bootstrap.min.js";
+function App() {
   return (
-    <div>
-      <h1>Hello jmmy!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="ui-app-layout">
+      <DataLayout></DataLayout>
+      {/* <Learning></Learning> */}
     </div>
   );
 }
+export default App;
